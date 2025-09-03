ed1e5464babf3975f3ce76b8d3a5c60d81074a1d
cTeam Ops <curiousteambd@gmail.com>
2025-09-03 10:56:40 +0600

    Added initial configs for all (mPDF, Dompdf, Html2Pdf) drivers.

---
+++ config/pdf.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'driver' => env('PDF_DRIVER', 'dompdf'), // dompdf|mpdf|html2pdf
+
+    'drivers' => [
+
+        'dompdf' => [
+            'options' => [
+                'isHtml5ParserEnabled' => true,
+                'isRemoteEnabled' => true,
+                'defaultFont' => 'DejaVu Sans',
+            ],
+            'paper' => ['size' => 'A4', 'orientation' => 'portrait'],
+        ],
+
+        'mpdf' => [
+            'options' => [
+                'mode' => 'utf-8',
+                'format' => 'A4',
+                'tempDir' => storage_path('app/mpdf-temp'),
+                'default_font' => 'dejavusans',
+            ],
+            'paper' => ['size' => 'A4', 'orientation' => 'portrait'],
+        ],
+
+        'html2pdf' => [
+            'options' => [
+                'format' => 'A4',
+                'orientation' => 'P',
+                'language' => 'en',
+                'unicode' => true,
+                'encoding' => 'UTF-8',
+                'margins' => [10, 10, 10, 10],
+            ],
+            'paper' => ['size' => 'A4', 'orientation' => 'portrait'],
+        ],
+
+    ],
+
+];
